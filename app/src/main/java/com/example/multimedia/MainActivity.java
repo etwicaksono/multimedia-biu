737b8a8c2676678a89ce4741e6704cd06bee2d9e
Eko Teguh Wicaksono <echotechno007@gmail.com>
2020-12-18 04:44:46 +0700

    Tambah sesuai ppt pertemuan 12

--- app/src/main/java/com/example/multimedia/MainActivity.java
+++ app/src/main/java/com/example/multimedia/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.multimedia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.multimedia.menu.AudioSederhanaActivity;
+import com.example.multimedia.menu.AudioStreamingActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
@@ -25,17 +28,39 @@ public class MainActivity extends AppCompatActivity {
         menu.setOnNavigationItemSelectedListener(listener);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()){
-                case R.id.nav_audio_sederhana:break;
-                case R.id.nav_audio_streaming:break;
-                case R.id.nav_video_sederhana:break;
-                case R.id.nav_video_streaming:break;
-            }
-            return false;
+/*ini adalah versi awal dari deklarasi variabel listener*/
+//    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.nav_audio_sederhana:
+//                    startActivity(new Intent(MainActivity.this, AudioSederhanaActivity.class));
+//                    break;
+//                case R.id.nav_audio_streaming:
+//                    break;
+//                case R.id.nav_video_sederhana:
+//                    break;
+//                case R.id.nav_video_streaming:
+//                    break;
+//            }
+//            return false;
+//        }
+//    };
+
+     private BottomNavigationView.OnNavigationItemSelectedListener listener = item -> {
+        switch (item.getItemId()) {
+            case R.id.nav_audio_sederhana:
+                startActivity(new Intent(MainActivity.this, AudioSederhanaActivity.class));
+                break;
+            case R.id.nav_audio_streaming:
+                startActivity(new Intent(MainActivity.this, AudioStreamingActivity.class));
+                break;
+            case R.id.nav_video_sederhana:
+                break;
+            case R.id.nav_video_streaming:
+                break;
         }
+        return false;
     };
 
 
--- app/src/main/java/com/example/multimedia/menu/AudioStreamingActivity.java
+++ app/src/main/java/com/example/multimedia/menu/AudioStreamingActivity.java
@@ -1,5 +1,6 @@
 package com.example.multimedia.menu;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,8 @@ import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.multimedia.R;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,12 +23,38 @@ public class AudioStreamingActivity extends AppCompatActivity {
     Button btnPlayStreaming;
     @BindView(R.id.btn_stop_streaming)
     Button btnStopStreaming;
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_streaming);
         ButterKnife.bind(this);
+        progressBarStream.setVisibility(View.VISIBLE);
+        progressBarStream.setIndeterminate(false);
+        progressBarStream.setMax(100);
+        prepareAudioStream();
+        btnPlayStreaming.setEnabled(true);
+        btnStopStreaming.setEnabled(false);
+    }
+
+    private void prepareAudioStream() {
+        String url = "https://202.147.199.99:8000/;";
+        mPlayer = new MediaPlayer();
+        try{
+            mPlayer.setDataSource(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                progressBarStream.setIndeterminate(true);
+                progressBarStream.setSecondaryProgress(100);
+            }
+        });
+
     }
 
     @OnClick({R.id.progress_bar_stream, R.id.btn_play_streaming, R.id.btn_stop_streaming})
@@ -34,8 +63,28 @@ public class AudioStreamingActivity extends AppCompatActivity {
             case R.id.progress_bar_stream:
                 break;
             case R.id.btn_play_streaming:
+                btnPlayStreaming.setEnabled(false);
+                btnStopStreaming.setEnabled(true);
+                progressBarStream.setVisibility(View.VISIBLE);
+                progressBarStream.setIndeterminate(true);
+                mPlayer.prepareAsync();
+                mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        mp.start();
+                        progressBarStream.setIndeterminate(false);
+                    }
+                });
                 break;
             case R.id.btn_stop_streaming:
+                if(mPlayer==null)return;
+                if(mPlayer.isPlaying()){
+                    btnPlayStreaming.setEnabled(true);
+                    btnStopStreaming.setEnabled(false);
+                    mPlayer.stop();
+                    mPlayer.release();
+                    prepareAudioStream();
+                }
                 break;
         }
     }
