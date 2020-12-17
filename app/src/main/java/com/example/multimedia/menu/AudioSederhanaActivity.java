bdbb564e02cb390a12671dee93e938ed04a3d35b
Eko Teguh Wicaksono <echotechno007@gmail.com>
2020-12-18 05:22:56 +0700

    Tambah sesuai docx pertemuan 12

--- app/src/main/java/com/example/multimedia/menu/AudioSederhanaActivity.java
+++ app/src/main/java/com/example/multimedia/menu/AudioSederhanaActivity.java
@@ -1,5 +1,6 @@
 package com.example.multimedia.menu;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.multimedia.R;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,24 +27,52 @@ public class AudioSederhanaActivity extends AppCompatActivity {
     Button btnResume;
     @BindView(R.id.btnStop)
     Button btnStop;
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_sederhana);
         ButterKnife.bind(this);
+        enable(btnPlay);
+        disable(btnPause,btnResume,btnStop);
     }
 
     @OnClick({R.id.btnPlay, R.id.btnPause, R.id.btnResume, R.id.btnStop})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btnPlay:
+                Uri uriMusik = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.music);
+                mPlayer=new MediaPlayer();
+                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                try {
+                    mPlayer.setDataSource(AudioSederhanaActivity.this,uriMusik);
+                    mPlayer.prepare();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                mPlayer.start();
+                enable(btnPause,btnStop);
+                disable(btnPlay,btnResume);
                 break;
             case R.id.btnPause:
+                if(mPlayer.isPlaying()){
+                    mPlayer.pause();
+                    enable(btnResume,btnStop);
+                    disable(btnPause,btnPlay);
+                }
                 break;
             case R.id.btnResume:
+                mPlayer.start();
+                enable(btnPause,btnStop);
+                disable(btnPlay,btnResume);
                 break;
             case R.id.btnStop:
+                if(mPlayer.isPlaying()&&mPlayer!=null){
+                    mPlayer.stop();
+                    enable(btnPlay);
+                    disable(btnResume,btnStop,btnPause);
+                }
                 break;
         }
     }
@@ -54,7 +85,7 @@ public class AudioSederhanaActivity extends AppCompatActivity {
 
     public void disable(View... v) {
         for (View mView : v) {
-            mView.setEnabled(true);
+            mView.setEnabled(false);
         }
     }
 
