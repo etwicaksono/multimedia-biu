71e37b74b052688991a88bba5a4d27f3609afcbd
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-01-24 11:25:39 +0700

    Sebelum dikompare dengan project Tyas

--- app/src/main/java/com/example/multimedia/MainActivity.java
+++ app/src/main/java/com/example/multimedia/MainActivity.java
@@ -25,43 +25,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         menu.setOnNavigationItemSelectedListener(listener);
     }
 
 /*ini adalah versi awal dari deklarasi variabel listener*/
-//    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.nav_audio_sederhana:
-//                    startActivity(new Intent(MainActivity.this, AudioSederhanaActivity.class));
-//                    break;
-//                case R.id.nav_audio_streaming:
-//                    break;
-//                case R.id.nav_video_sederhana:
-//                    break;
-//                case R.id.nav_video_streaming:
-//                    break;
-//            }
-//            return false;
-//        }
-//    };
-
-     private BottomNavigationView.OnNavigationItemSelectedListener listener = item -> {
-        switch (item.getItemId()) {
+private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+        switch (menuitem.getItemId()){
             case R.id.nav_audio_sederhana:
                 startActivity(new Intent(MainActivity.this, AudioSederhanaActivity.class));
                 break;
-            case R.id.nav_audio_streaming:
-                startActivity(new Intent(MainActivity.this, AudioStreamingActivity.class));
-                break;
-            case R.id.nav_video_sederhana:
-                break;
-            case R.id.nav_video_streaming:
-                break;
         }
         return false;
-    };
+    }
+};
+
+
+//     private BottomNavigationView.OnNavigationItemSelectedListener listener = item -> {
+//        switch (item.getItemId()) {
+//            case R.id.nav_audio_sederhana:
+//                startActivity(new Intent(MainActivity.this, AudioSederhanaActivity.class));
+//                break;
+//            case R.id.nav_audio_streaming:
+//                startActivity(new Intent(MainActivity.this, AudioStreamingActivity.class));
+//                break;
+////            case R.id.nav_video_sederhana:
+////                break;
+////            case R.id.nav_video_streaming:
+////                break;
+//        }
+//        return false;
+//    };
 
 
 }
